92909e5d16ce8f2983cf492df404dfbc9f789290
Andi Eder <aeder@hsr.ch>
2017-11-17 12:29:41 +0100

    Removed empty methods and inheritance from object

--- elbotto/bots/training/training.py
+++ elbotto/bots/training/training.py
@@ -1,14 +1,16 @@
+from abc import abstractmethod
 from json import dump
 
 
-class Training(object):
+class Training:
     def __init__(self, name):
         self.name = name
         self.q_model = None
         self.define_model()
 
+    @abstractmethod
     def define_model(self):
-        pass
+        raise NotImplementedError
 
     def save_weights(self, path):
         self.q_model.save_weights(path)
@@ -24,14 +26,3 @@ class Training(object):
             self.q_model.save(path)
             save_type = 'h5'
         return print("The model saved as " + save_type + ".")
-
-    @staticmethod
-    def create_input():
-        pass
-
-    @staticmethod
-    def create_target():
-        pass
-
-    def train_the_model(self):
-        pass
