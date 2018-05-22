5d8a8e672f314175395d63773747d41ac390c299
Andi Eder <aeder@hsr.ch>
2018-05-22 19:01:52 +0200

    Refactoring manual trumpf tests

--- elbotto/bots/training/manual_trumpf_testing.py
+++ elbotto/bots/training/manual_trumpf_testing.py
@@ -1,18 +1,5 @@
 import numpy as np
 from elbotto.bots.training.manual_testing import is_none, get_model, create_test_matrix
-# from elbotto.bots.training.trumpf_converter import trumpf_converter
-# from elbotto.bots.training.trumpf_training import create_input
-
-
-# def create_test_matrix(hand_cards, pushed_from_partner=False):
-#     if isinstance(pushed_from_partner, bool) and pushed_from_partner:
-#         shift = trumpf_converter(6)
-#     else:
-#         shift = trumpf_converter(0)
-#
-#     hand_card_list = fillup_card_list(hand_cards)
-#
-#     return create_input(hand_card_list, shift)
 
 
 def manuel_test_input_predict(model=None, hand_cards=None, pushed=False, safe_in_textfile=False):
@@ -20,11 +7,7 @@ def manuel_test_input_predict(model=None, hand_cards=None, pushed=False, safe_in
 
     t_model = get_model(model)
 
-    print(hand_cards)
-
     input_matrix = create_test_matrix(hand_cards, pushed)
-
-    print(input_matrix)
 
     if input_matrix is None:
         print("Your input is wrong. Please check your cards!")
@@ -32,9 +15,8 @@ def manuel_test_input_predict(model=None, hand_cards=None, pushed=False, safe_in
         result = t_model.predict(np.asarray(input_matrix))
         print(
             'The prediction is: \n'
-            ' hearts: {} \n diamonds: {} \n clubs: {} \n spades: {} \n OBEABE: {} \n UNDEUFE: {} \n SCHIEBE: {}'
-                .format(result[0][0], result[0][1], result[0][2], result[0][3],
-                        result[0][4], result[0][5], result[0][6]))
+            ' hearts: {} \n diamonds: {} \n clubs: {} \n spades: {} \n OBEABE: {} \n UNDEUFE: {} \n SCHIEBE: {}'.format
+            (result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5], result[0][6]))
         if safe_in_textfile:
             safe_as_txtfile(hand_cards, result)
 
@@ -52,8 +34,10 @@ def safe_as_txtfile(hand_cards, result, filename="result_Trumpfwahl.csv"):
     fobj_out.close()
 
 
-# Input your hand cards into the list and set 'pushed' to True if your partner moved the trump decision to you.
-# Set with model that model you want to test.
+# Set with 'model' that model you want to test.
+# Input your hand cards into the list 'hand_cards'
+# Set 'pushed' to True if your partner moved the trump decision to you.
+# Set 'safe_in_textfile' if you want safe the results in a Textfile
 if __name__ == '__main__':
     manuel_test_input_predict(model="./config/trumpf_network_model_final__2018-05-09_101936.h5",
                               hand_cards=["H6", "C8", "H9", "HJ", "D8", "D9", "D10", "S8", "S10"],
--- elbotto/bots/training/manual_trumpf_testing_from_list.py
+++ elbotto/bots/training/manual_trumpf_testing_from_list.py
@@ -1,19 +1,6 @@
 import numpy as np
 from elbotto.bots.training.manual_testing import is_none, get_model, create_test_matrix
-# from elbotto.bots.training.trumpf_converter import trumpf_converter
-# from elbotto.bots.training.trumpf_training import create_input
 from openpyxl import load_workbook, Workbook
-
-
-# def create_test_matrix(hand_cards, pushed_from_partner=False): #extract method
-#     if isinstance(pushed_from_partner, bool) and pushed_from_partner:
-#         shift = trumpf_converter(6)
-#     else:
-#         shift = trumpf_converter(0)
-#
-#     hand_card_list = fillup_card_list(hand_cards)
-#
-#     return create_input(hand_card_list, shift)
 
 
 def manuel_test_list_input_predict(import_list_file, model=None):
@@ -26,8 +13,8 @@ def manuel_test_list_input_predict(import_list_file, model=None):
 
     hand_cards_matrix = extract_lines_from_sheet(input_sheet_matrix)
 
-    print('handcards: {}'.format(hand_cards_matrix))
-    card_matrix = shrink_to_handcards(hand_cards_matrix)
+    # print('handcards: {}'.format(hand_cards_matrix))
+    card_matrix = shrink_lines_to_handcards(hand_cards_matrix)
 
     prediction_matrix = prepared_prediction_matrix(card_matrix)
 
@@ -43,9 +30,8 @@ def calc_prediction_matrix(prediction_matrix, t_model):
         result_list = matrix_to_list(result)
         print(
             'The prediction is: \n'
-            ' hearts: {} \n diamonds: {} \n clubs: {} \n spades: {} \n OBEABE: {} \n UNDEUFE: {} \n SCHIEBE: {}'
-                .format(result[0][0], result[0][1], result[0][2], result[0][3],
-                        result[0][4], result[0][5], result[0][6]))
+            ' hearts: {} \n diamonds: {} \n clubs: {} \n spades: {} \n OBEABE: {} \n UNDEUFE: {} \n SCHIEBE: {}'.format
+            (result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5], result[0][6]))
         results.append(result_list)
     return results
 
@@ -58,14 +44,11 @@ def prepared_prediction_matrix(card_matrix):
     return prediction_matrix
 
 
-def shrink_to_handcards(hand_cards_matrix):
+def shrink_lines_to_handcards(hand_cards_matrix):
     card_matrix = []
     for start_cards in hand_cards_matrix:
         if is_none(start_cards[0]):
             card_matrix.append(start_cards)
-            print('Output the input_matrix: {}'.format(card_matrix))
-        else:
-            print("It's a None-Entry!!!")
     return card_matrix
 
 
@@ -76,8 +59,6 @@ def extract_lines_from_sheet(input_sheet_matrix):
         input_list = []
         for cell in row:
             input_list.append(cell.value)
-
-        print('InputListe: {}'.format(input_list))
 
         if is_none(input_list[len(input_list) - 1]):
             input_list[len(input_list) - 1] = True
@@ -92,7 +73,6 @@ def matrix_to_list(result):
     new_list = []
     for r in result[0]:
         new_list.append(r)
-    print('New List: {}'.format(new_list))
     return new_list
 
 
@@ -116,10 +96,8 @@ def fillup_table(hand_cards, length_of_output_matrix, results, ws):
         i = 0
         for cell in row:
             if i < len(hand_cards[0]):
-                print(hand_cards[matrix_line_index][i])
                 cell.value = hand_cards[matrix_line_index][i]
             else:
-                print('{}'.format(results[matrix_line_index][i - len(hand_cards[0])]))
                 cell.value = results[matrix_line_index][i - len(hand_cards[0])]
             i += 1
         matrix_line_index += 1
@@ -137,8 +115,12 @@ def create_table_title(length_of_output_matrix, ws):
 
 
 # Input your hand cards into a excel list as the template excel sheet
-# Set also in the list template in the column 'Geschoben?' or 'pushed?' a 'x' if your partner moved the trump decision to you.
+# Set also in the list template in the column 'Geschoben?' or 'pushed?' a 'x'
+# if your partner moved the trumpf decision to you.
+#
 # Set with model that model you want to test.
+#
+# Result: the function write the results in the file './data/trumpf_cards/result_Trumpfwahl.xlsx'
 if __name__ == '__main__':
     manuel_test_list_input_predict(import_list_file="./data/trumpf_list.xlsx",
                                    model="./config/trumpf_network_model_final__2018-05-09_101936.h5")
